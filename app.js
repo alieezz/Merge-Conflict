bc525f5309b92ea15a8fce19cc4395141b2b6042
alieezzz <ali.haider18@hotmail.com>
2024-05-02 20:44:27 -0700

    Merge Commit

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var a,b;
-a=10, b=56;
+a=100, b=5;
 var res=a+b;
 console.log(res)
 console.log(a);
 console.log(b);
 
 
+ 
+
 
 //int c=123
 //git practice is all the way 
 //Merge conflict 
-//another
+//another conflict
